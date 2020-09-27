9672fc0a8b57008a1d769f37062a1e5ce3b2cad8
Josh <=>
2020-09-27 16:42:16 +0100

    wip API interface for tide times

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,33 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/stations/{stationID}', function ($stationID) {
+    // $station = Station::where('id', $stationID)->findOrFail();
+    
+    // currently the API doesn't work - keep getting 401'd
+    $apiURL = env('admirality_api_url', false);
+
+    $apiKey =  env('admirality_api_key', false);
+
+    if (!$apiURL || !$apiKey)
+    {
+        throw new Exception('Unable to initialise external API Client: invalid credentials');
+    }
+
+    // clean the param first
+    $apiURL .= "/{$stationID}";
+
+    $client = new GuzzleHttp\Client();
+
+    $headers = ['Ocp-Apim-Subscription-Key' => $apiKey];
+    
+    $res = $client->request('GET', $apiURL, $headers);
+
+    echo $res->getStatusCode();
+    // "200"
+    echo $res->getHeader('content-type')[0];
+    // 'application/json; charset=utf8'
+    echo $res->getBody();
+});
