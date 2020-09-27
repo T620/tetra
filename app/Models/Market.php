35646bb1024baf240a46244ce474c1ccea048793
Josh <=>
2020-09-27 10:29:40 +0100

    add slugs to markets

--- app/Models/Market.php
+++ app/Models/Market.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 use Enums;
+use Illuminate\Support\Str;
 
 class Market extends Model
 {
@@ -40,5 +41,16 @@ class Market extends Model
 
         // this market is well known for selling, Trout for example, which is a Species. However, they sell many sizes of trout and at different prices so we need one Species to then link to many products (the sizes of that fish in the real world)
         return $this->hasOne(Species::class, 'id');
+    }
+
+    public function slug()
+    {
+        // I couldn't get the seeder to work but this does the job. If the slug's empty, generate it, save it and return it 
+        if (empty($this->slug)) {
+            $this->slug = (string) Str::of($this->name)->slug('-');
+            $this->update();
+        }
+
+        return $this->slug;
     }
 }
