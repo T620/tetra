1990a16ea90881d9d823afb611bbdbf007865d4f
Josh <=>
2020-09-27 10:33:32 +0100

    add user management/authentication with Livewire/Jetstream and tailwind UI

--- webpack.mix.js
+++ webpack.mix.js
@@ -9,12 +9,10 @@ const mix = require('laravel-mix');
  | for your Laravel applications. By default, we are compiling the CSS
  | file for the application as well as bundling up all the JS files.
  |
-//  */
+ */
 
-// mix.js('resources/js/app.js', 'public/js')
-//     .postCss('resources/css/app.css', 'public/css', [
-//         //
-//     ]);
-
-
-mix.sass('resources/scss/app.scss', 'public/css/');
+mix.js('resources/js/app.js', 'public/js')
+    .postCss('resources/css/app.css', 'public/css', [
+        require('postcss-import'),
+        require('tailwindcss'),
+    ]);
