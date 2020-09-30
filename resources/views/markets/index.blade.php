a7b136fe0f41385d4ee3b52e5e7046d4d2e0f72a
Josh <=>
2020-09-30 19:55:53 +0100

    rm mrkets/index.blade.php

--- resources/views/markets/index.blade.php
+++
@@ -1,9 +0,0 @@
-
-<h1>Markets</h1>
-<ul>
-@foreach ($markets as $market)
-    {{-- use url helper --}}
-    <li><a href="/markets/{{$market->id}}">{{ $market->name }} </a></li>
-@endforeach
-</ul>
-
